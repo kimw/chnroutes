75c5f557f8a31c8e8770237456a58edc109881d6
Kim Wong <1@kimwong.me>
2014-10-26 12:41:53 +0800

    Reformat the code style follow 'Google Python Style Guide'.

--- chnroutes.py
+++ chnroutes.py
@@ -1,250 +1,284 @@
 #!/usr/bin/env python
+#vim: set ts=8 et sw=4 sts=4
 
-import re
-import urllib2
-import sys
+"""
+scripts help chinese netizen, who uses vpn to combat censorship, by modifying
+the route table so as routing only the censored ip to the vpn.
+"""
+
 import argparse
 import math
-import textwrap
+import re
+import sys
+import urllib2
 
 
 def generate_ovpn(metric):
-    results = fetch_ip_data()  
-    rfile=open('routes.txt','w')
-    for ip,mask,_ in results:
-        route_item="route %s %s net_gateway %d\n"%(ip,mask,metric)
+    """
+    TODO (kim): add function docstring
+    """
+
+    results = fetch_ip_data()
+    rfile = open('routes.txt', 'w')
+    for ip, mask, _ in results:  #pylint: disable=invalid-name
+        route_item = 'route %s %s net_gateway %d\n' % (ip, mask, metric)
         rfile.write(route_item)
     rfile.close()
-    print "Usage: Append the content of the newly created routes.txt to your openvpn config file," \
-          " and also add 'max-routes %d', which takes a line, to the head of the file." % (len(results)+20)
+    print 'Usage: Append the content of the newly created routes.txt to your' \
+          ' openvpn config file, and also add "max-routes %d", which takes a' \
+          ' line, to the head of the file.' % (len(results) + 20)
 
 
-def generate_linux(metric):
+def generate_linux(metric):  #pylint: disable=unused-argument
+    """
+    TODO (kim): add function docstring
+    """
+
     results = fetch_ip_data()
-    upscript_header=textwrap.dedent("""\
-    #!/bin/bash
-    export PATH="/bin:/sbin:/usr/sbin:/usr/bin"
-    
-    OLDGW=`ip route show | grep '^default' | sed -e 's/default via \\([^ ]*\\).*/\\1/'`
-    
-    if [ $OLDGW == '' ]; then
-        exit 0
-    fi
-    
-    if [ ! -e /tmp/vpn_oldgw ]; then
-        echo $OLDGW > /tmp/vpn_oldgw
-    fi
-    
-    """)
-    
-    downscript_header=textwrap.dedent("""\
-    #!/bin/bash
-    export PATH="/bin:/sbin:/usr/sbin:/usr/bin"
-    
-    OLDGW=`cat /tmp/vpn_oldgw`
-    
-    """)
-    
-    upfile=open('ip-pre-up','w')
-    downfile=open('ip-down','w')
-    
+    upscript_header = (
+        '#!/bin/bash\n'
+        'export PATH="/bin:/sbin:/usr/sbin:/usr/bin"\n'
+        '\n'
+        'OLDGW=`ip route show'
+            ' | grep \'^default\''
+            ' | sed -e \'s/default via \\([^ ]*\\).*/\\1/\'`\n'
+        '\n'
+        'if [ $OLDGW == \'\' ]; then\n'
+        '    exit 0\n'
+        'fi\n'
+        '\n'
+        'if [ ! -e /tmp/vpn_oldgw ]; then\n'
+        '    echo $OLDGW > /tmp/vpn_oldgw\n'
+        'fi\n'
+        '\n')
+
+    downscript_header = (
+        '#!/bin/bash\n'
+        'export PATH="/bin:/sbin:/usr/sbin:/usr/bin"\n'
+        '\n'
+        'OLDGW=`cat /tmp/vpn_oldgw`\n'
+        '\n')
+
+    upfile = open('ip-pre-up', 'w')
+    downfile = open('ip-down', 'w')
+
     upfile.write(upscript_header)
-    upfile.write('\n')
     downfile.write(downscript_header)
-    downfile.write('\n')
-    
-    for ip,mask,_ in results:
-        upfile.write('route add -net %s netmask %s gw $OLDGW\n'%(ip,mask))
-        downfile.write('route del -net %s netmask %s\n'%(ip,mask))
+
+    for ip, mask, _ in results:  #pylint: disable=invalid-name
+        upfile.write('route add -net %s netmask %s gw $OLDGW\n' % (ip, mask))
+        downfile.write('route del -net %s netmask %s\n' % (ip, mask))
 
     downfile.write('rm /tmp/vpn_oldgw\n')
 
 
-    print "For pptp only, please copy the file ip-pre-up to the folder/etc/ppp," \
-          "and copy the file ip-down to the folder /etc/ppp/ip-down.d."
+    print ('For pptp only, please copy the file ip-pre-up to the folder'
+           ' "/etc/ppp", and copy the file ip-down to the folder'
+           ' "/etc/ppp/ip-down.d".')
 
-def generate_mac(metric):
-    results=fetch_ip_data()
-    
-    upscript_header=textwrap.dedent("""\
-    #!/bin/sh
-    export PATH="/bin:/sbin:/usr/sbin:/usr/bin"
-    
-    OLDGW=`netstat -nr | grep '^default' | grep -v 'ppp' | sed 's/default *\\([0-9\.]*\\) .*/\\1/' | awk '{if($1){print $1}}'`
 
-    if [ ! -e /tmp/pptp_oldgw ]; then
-        echo "${OLDGW}" > /tmp/pptp_oldgw
-    fi
-    
-    dscacheutil -flushcache
+def generate_mac(metric):  #pylint: disable=unused-argument
+    """
+    TODO (kim): add function docstring
+    """
 
-    route add 10.0.0.0/8 "${OLDGW}"
-    route add 172.16.0.0/12 "${OLDGW}"
-    route add 192.168.0.0/16 "${OLDGW}"
-    """)
-    
-    downscript_header=textwrap.dedent("""\
-    #!/bin/sh
-    export PATH="/bin:/sbin:/usr/sbin:/usr/bin"
-    
-    if [ ! -e /tmp/pptp_oldgw ]; then
-            exit 0
-    fi
-    
-    ODLGW=`cat /tmp/pptp_oldgw`
+    results = fetch_ip_data()
 
-    route delete 10.0.0.0/8 "${OLDGW}"
-    route delete 172.16.0.0/12 "${OLDGW}"
-    route delete 192.168.0.0/16 "${OLDGW}"
-    """)
-    
-    upfile=open('ip-up','w')
-    downfile=open('ip-down','w')
-    
+    upscript_header = (
+        '#!/bin/sh\n'
+        'export PATH="/bin:/sbin:/usr/sbin:/usr/bin"\n'
+        '\n'
+        'OLDGW=`netstat -nr'
+            ' | grep \'^default\''
+            ' | grep -v \'ppp\''
+            ' | sed \'s/default *\\([0-9\.]*\\) .*/\\1/\''
+            ' | awk \'{if($1){print $1}}\'`\n'
+        '\n'
+        'if [ ! -e /tmp/pptp_oldgw ]; then\n'
+        '    echo "${OLDGW}" > /tmp/pptp_oldgw\n'
+        'fi\n'
+        '\n'
+        'dscacheutil -flushcache\n'
+        '\n'
+        'route add 10.0.0.0/8 "${OLDGW}"\n'
+        'route add 172.16.0.0/12 "${OLDGW}"\n'
+        'route add 192.168.0.0/16 "${OLDGW}"\n')
+
+    downscript_header = (
+        '#!/bin/sh\n'
+        'export PATH="/bin:/sbin:/usr/sbin:/usr/bin"\n'
+        '\n'
+        'if [ ! -e /tmp/pptp_oldgw ]; then\n'
+        '    exit 0\n'
+        'fi\n'
+        '\n'
+        'ODLGW=`cat /tmp/pptp_oldgw`\n'
+        '\n'
+        'route delete 10.0.0.0/8 "${OLDGW}"\n'
+        'route delete 172.16.0.0/12 "${OLDGW}"\n'
+        'route delete 192.168.0.0/16 "${OLDGW}"\n')
+
+    upfile = open('ip-up', 'w')
+    downfile = open('ip-down', 'w')
+
     upfile.write(upscript_header)
-    upfile.write('\n')
     downfile.write(downscript_header)
-    downfile.write('\n')
-    
-    for ip,_,mask in results:
-        upfile.write('route add %s/%s "${OLDGW}"\n'%(ip,mask))
-        downfile.write('route delete %s/%s ${OLDGW}\n'%(ip,mask))
-    
+
+    for ip, _, mask in results:  #pylint: disable=invalid-name
+        upfile.write('route add %s/%s "${OLDGW}"\n' % (ip, mask))
+        downfile.write('route delete %s/%s ${OLDGW}\n' % (ip, mask))
+
     downfile.write('\n\nrm /tmp/pptp_oldgw\n')
     upfile.close()
     downfile.close()
-    
-    print "For pptp on mac only, please copy ip-up and ip-down to the /etc/ppp folder," \
-          "don't forget to make them executable with the chmod command."
+
+    print ('For pptp on mac only, please copy ip-up and ip-down to the /etc/ppp'
+           ' folder, don\'t forget to make them executable with the chmod'
+           ' command.')
+
 
 def generate_win(metric):
-    results = fetch_ip_data()  
+    """
+    TODO (kim): add function docstring
+    """
 
-    upscript_header=textwrap.dedent("""@echo off
-    for /F "tokens=3" %%* in ('route print ^| findstr "\\<0.0.0.0\\>"') do set "gw=%%*"
-    
-    """)
-    
-    upfile=open('vpnup.bat','w')
-    downfile=open('vpndown.bat','w')
-    
-    upfile.write(upscript_header)
-    upfile.write('\n')
-    upfile.write('ipconfig /flushdns\n\n')
-    
-    downfile.write("@echo off")
-    downfile.write('\n')
-    
-    for ip,mask,_ in results:
-        upfile.write('route add %s mask %s %s metric %d\n'%(ip,mask,"%gw%",metric))
-        downfile.write('route delete %s\n'%(ip))
-    
-    upfile.close()
-    downfile.close()
-    
-#    up_vbs_wrapper=open('vpnup.vbs','w')
-#    up_vbs_wrapper.write('Set objShell = CreateObject("Wscript.shell")\ncall objShell.Run("vpnup.bat",0,FALSE)')
-#    up_vbs_wrapper.close()
-#    down_vbs_wrapper=open('vpndown.vbs','w')
-#    down_vbs_wrapper.write('Set objShell = CreateObject("Wscript.shell")\ncall objShell.Run("vpndown.bat",0,FALSE)')
-#    down_vbs_wrapper.close()
-    
-    print "For pptp on windows only, run vpnup.bat before dialing to vpn," \
-          "and run vpndown.bat after disconnected from the vpn."
-
-def generate_android(metric):
     results = fetch_ip_data()
-    
-    upscript_header=textwrap.dedent("""\
-    #!/bin/sh
-    alias nestat='/system/xbin/busybox netstat'
-    alias grep='/system/xbin/busybox grep'
-    alias awk='/system/xbin/busybox awk'
-    alias route='/system/xbin/busybox route'
-    
-    OLDGW=`netstat -rn | grep ^0\.0\.0\.0 | awk '{print $2}'`
-    
-    """)
-    
-    downscript_header=textwrap.dedent("""\
-    #!/bin/sh
-    alias route='/system/xbin/busybox route'
-    
-    """)
-    
-    upfile=open('vpnup.sh','w')
-    downfile=open('vpndown.sh','w')
-    
+
+    upscript_header = (
+        '@echo off\n'
+        'for /F "tokens=3" %%* in (\'route print ^| findstr "\\<0.0.0.0\\>"\')'
+            ' do set "gw=%%*"\n'
+        '\n')
+
+    upfile = open('vpnup.bat', 'w')
+    downfile = open('vpndown.bat', 'w')
+
     upfile.write(upscript_header)
-    upfile.write('\n')
-    downfile.write(downscript_header)
-    downfile.write('\n')
-    
-    for ip,mask,_ in results:
-        upfile.write('route add -net %s netmask %s gw $OLDGW\n'%(ip,mask))
-        downfile.write('route del -net %s netmask %s\n'%(ip,mask))
-    
+    upfile.write('ipconfig /flushdns\n\n')
+
+    downfile.write('@echo off\n')
+
+    for ip, mask, _ in results:  #pylint: disable=invalid-name
+        upfile.write('route add %s mask %s %s metric %d\n'
+            % (ip, mask, "%gw%", metric))
+        downfile.write('route delete %s\n' % (ip))
+
     upfile.close()
     downfile.close()
-    
-    print "Old school way to call up/down script from openvpn client. " \
-          "use the regular openvpn 2.1 method to add routes if it's possible"
+
+    print ('For pptp on windows only, run vpnup.bat before dialing to vpn,'
+           ' and run vpndown.bat after disconnected from the vpn.')
+
+
+def generate_android(metric):  #pylint: disable=unused-argument
+    """
+    TODO (kim): add function docstring
+    """
+
+    results = fetch_ip_data()
+
+    upscript_header = (
+        '#!/bin/sh\n'
+        'alias nestat=\'/system/xbin/busybox netstat\'\n'
+        'alias grep=\'/system/xbin/busybox grep\'\n'
+        'alias awk=\'/system/xbin/busybox awk\'\n'
+        'alias route=\'/system/xbin/busybox route\'\n'
+        '\n'
+        'OLDGW=`netstat -rn | grep ^0\.0\.0\.0 | awk \'{print $2}\'`\n'
+        '\n')
+
+    downscript_header = (
+        '#!/bin/sh\n'
+        'alias route=\'/system/xbin/busybox route\'\n'
+        '\n')
+
+    upfile = open('vpnup.sh', 'w')
+    downfile = open('vpndown.sh', 'w')
+
+    upfile.write(upscript_header)
+    downfile.write(downscript_header)
+
+    for ip, mask, _ in results:  #pylint: disable=invalid-name
+        upfile.write('route add -net %s netmask %s gw $OLDGW\n' % (ip, mask))
+        downfile.write('route del -net %s netmask %s\n' % (ip, mask))
+
+    upfile.close()
+    downfile.close()
+
+    print ('Old school way to call up/down script from openvpn client.'
+           ' use the regular openvpn 2.1 method to add routes if it\'s'
+           ' possible.')
 
 
 def fetch_ip_data():
+    """
+    TODO (kim): add function docstring
+    """
+
     #fetch data from apnic
-    print "Fetching data from apnic.net, it might take a few minutes, please wait..."
-    url=r'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest'
-    data=urllib2.urlopen(url).read()
-    
-    cnregex=re.compile(r'apnic\|cn\|ipv4\|[0-9\.]+\|[0-9]+\|[0-9]+\|a.*',re.IGNORECASE)
-    cndata=cnregex.findall(data)
-    
-    results=[]
+    print ('Fetching data from apnic.net, it might take a few minutes,'
+           ' please wait...')
+    url = r'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest'
+    data = urllib2.urlopen(url).read()
+
+    cnregex = re.compile(
+        r'apnic\|cn\|ipv4\|[0-9\.]+\|[0-9]+\|[0-9]+\|a.*',
+        re.IGNORECASE)
+    cndata = cnregex.findall(data)
+
+    results = []
 
     for item in cndata:
-        unit_items=item.split('|')
-        starting_ip=unit_items[3]
-        num_ip=int(unit_items[4])
-        
-        imask=0xffffffff^(num_ip-1)
+        unit_items = item.split('|')
+        starting_ip = unit_items[3]
+        num_ip = int(unit_items[4])
+
+        imask = 0xffffffff ^ (num_ip - 1)
         #convert to string
-        imask=hex(imask)[2:]
-        mask=[0]*4
-        mask[0]=imask[0:2]
-        mask[1]=imask[2:4]
-        mask[2]=imask[4:6]
-        mask[3]=imask[6:8]
-        
+        imask = hex(imask)[2:]
+        mask = [0]*4
+        mask[0] = imask[0:2]
+        mask[1] = imask[2:4]
+        mask[2] = imask[4:6]
+        mask[3] = imask[6:8]
+
         #convert str to int
-        mask=[ int(i,16 ) for i in mask]
-        mask="%d.%d.%d.%d"%tuple(mask)
-        
+        mask = [int(i, 16) for i in mask]
+        mask = '%d.%d.%d.%d' % tuple(mask)
+
         #mask in *nix format
-        mask2=32-int(math.log(num_ip,2))
-        
-        results.append((starting_ip,mask,mask2))
-         
+        mask2 = 32 - int(math.log(num_ip, 2))
+
+        results.append((starting_ip, mask, mask2))
+
     return results
 
 
-if __name__=='__main__':
-    parser=argparse.ArgumentParser(description="Generate routing rules for vpn.")
-    parser.add_argument('-p','--platform',
-                        dest='platform',
-                        default='openvpn',
-                        nargs='?',
-                        help="Target platforms, it can be openvpn, mac, linux," 
-                        "win, android. openvpn by default.")
-    parser.add_argument('-m','--metric',
-                        dest='metric',
-                        default=5,
-                        nargs='?',
-                        type=int,
-                        help="Metric setting for the route rules")
-    
+def main():
+    """
+    TODO (kim): add function docstring
+    """
+
+    parser = argparse.ArgumentParser(
+        description='Generate routing rules for vpn.')
+    parser.add_argument(
+        '-p',
+        '--platform',
+        dest='platform',
+        default='openvpn',
+        nargs='?',
+        help=('Target platforms, it can be:\n openvpn, mac, linux, win,'
+              ' or android. openvpn by default.'))
+    parser.add_argument(
+        '-m', '--metric',
+        dest='metric',
+        default=5,
+        nargs='?',
+        type=int,
+        help='Metric setting for the route rules.')
+
     args = parser.parse_args()
-    
+
     if args.platform.lower() == 'openvpn':
         generate_ovpn(args.metric)
     elif args.platform.lower() == 'linux':
@@ -256,5 +290,9 @@ if __name__=='__main__':
     elif args.platform.lower() == 'android':
         generate_android(args.metric)
     else:
-        print>>sys.stderr, "Platform %s is not supported."%args.platform
+        print >> sys.stderr, 'Platform %s is not supported.' % args.platform
         exit(1)
+
+
+if __name__ == '__main__':
+    main()
